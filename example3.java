1e929ae9adc1b33396b26cbae6cc21bd21f507e6
dspiegelman <mtvesuvius67@gmail.com>
2024-02-08 14:55:01 -0500

    Full code

---
+++ example3.java
@@ -0,0 +1,10 @@
+public class example3 {
+    public static void main(String[]args) {
+        System.out.println("Delia Spiegelman");
+        System.out.println("");
+        System.out.println("Poem: \"How much will you pay for an extra day? He asked when the time came to die");
+        System.out.println("      \"All of the pearls in all of the seas, and all of the stars in the sky\"\"");
+        System.out.println("\t- Shel Silverstein");
+        System.out.println("Proverb: \"The pen is mightier than the sword\"");
+    }
+}
